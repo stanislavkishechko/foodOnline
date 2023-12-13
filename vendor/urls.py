d5e944cd4ab284e968ff546fc3dbed03fc36d03b
Stanislav Kishechko <stanislavkishechko@gmail.com>
2023-12-13 21:58:00 +0200

    Category CRUD operations

--- vendor/urls.py
+++ vendor/urls.py
@@ -6,4 +6,9 @@ from accounts import views as AccountViews
 urlpatterns = [
     path('', AccountViews.vendorDashboard, name='vendor'),
     path('profile/', views.vprofile, name='vprofile'),
+    path('menu-builder/', views.menu_builder, name='menu-builder'),
+    path('menu-builder/category/<int:pk>/', views.fooditems_by_category, name='fooditems-by-category'),
+    path('menu-builder/category/add/', views.add_category, name='add-category'),
+    path('menu-builder/category/edit/<int:pk>/', views.edit_category, name='edit-category'),
+    path('menu-builder/category/delete/<int:pk>/', views.delete_category, name='delete-category'),
 ]
